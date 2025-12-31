06fe4f049f1d4effe62727c019169ff9fa391f68
SparrowZA <geffroymarc@gmail.com>
2026-01-01 00:23:34 +0200

    Implemented line count

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ def main():
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-c')
+    parser.add_argument('-l')
     args = parser.parse_args()
 
-    if hasattr(args, "c"):
+    if hasattr(args, "c") and args.c is not None:
         file_name = args.c
-
-    with open(file_name, 'r') as fd:
-        file = fd.read()
-    
-    print(f"{len(file)} {file_name}")
+        with open(file_name, 'r') as fd:
+            file = fd.read()
+        print(f"{len(file)} {file_name}")
+    if hasattr(args, 'l') and args.l is not None:
+        file_name = args.l
+        with open(file_name, 'r') as fd:
+            lines = fd.readlines()
+        print(f'{len(lines)} {file_name}')
 
 if __name__ == "__main__":
     main()
